a3c4daae9a88192446d565c164591305aa6a227f
Leonardo Santiago <leogiuris@gmail.com>
2022-01-29 18:03:33 -0300

    tem timer

---
+++ CB_GlobalGJ2022/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float init;
+
+
+    public void StartTimer()
+    {
+        init = Time.timeSinceLevelLoad;
+    }
+
+    public float getTime()
+    {
+        return Time.timeSinceLevelLoad - init;
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
