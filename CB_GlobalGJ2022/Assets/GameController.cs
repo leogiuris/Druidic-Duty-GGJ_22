61d4e4820251c5dd6b3f48b1a99652ef6357b514
Leonardo Santiago <leogiuris@gmail.com>
2022-01-28 22:34:57 -0300

    mamadinha

--- CB_GlobalGJ2022/Assets/GameController.cs
+++ CB_GlobalGJ2022/Assets/GameController.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-
+    int maxInitSpawn = 5;
     public float ratioHerb;
     public int herbQtd;
     public int predQtd;
 
-    int herb_init_spawns = 5;
+    int herb_init_spawns = 1;
 
-
-
-    GameObject Herb;
-    GameObject Pred;
+    Vector3[] herbSpawnLocations;
+    public GameObject Herb;
+    public GameObject Pred;
     
     void BeginGame()
     {
         int i = 0;
         while (i < herb_init_spawns)
         {
-            GameObject initHerb = Instantiate(Herb, transform.position, transform.rotation);
+            GameObject initHerb = Instantiate(Herb, herbSpawnLocations[i], transform.rotation);
             i++;
         }
         i = 0;
@@ -44,6 +43,12 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        herbSpawnLocations = new Vector3[maxInitSpawn];
+        for(int i = 0; i<maxInitSpawn; i++)
+        {
+            herbSpawnLocations[i] = new Vector3((Random.value * 40) - 20, (Random.value * 40) - 20);
+        }
+
         BeginGame();
     }
 
