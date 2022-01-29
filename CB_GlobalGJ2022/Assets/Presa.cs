83020cf0d0a30fee27db26cb307ae8ad24da16dc
brumach <brumach527@gmail.com>
2022-01-29 01:27:03 -0300

    funicional

--- CB_GlobalGJ2022/Assets/Presa.cs
+++ CB_GlobalGJ2022/Assets/Presa.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class Presa : Animal
 {
-
+    public GameObject sPresa;
     bool isFleeing;
 
     void EatVeg(GameObject planta)
     {
-
+        hunger--;
+        Destroy(planta);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Arvore")
-        {
-            EatVeg(collision.gameObject);
-        }
         if (collision.transform.tag == "Terreno")
         {
             flipper = flipper * -1;
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "arvore")
+        {
+            EatVeg(collision.gameObject);
+            gC.SpawnaAnimal(sPresa, transform.position);
+        }
+    }
 
-}
+    }
