6612840823c409f0299e24daf452879583e29e8a
Leonardo Santiago <leogiuris@gmail.com>
2022-01-30 00:11:03 -0300

    tempo e camera better

--- CB_GlobalGJ2022/Assets/Scripts/CameraManager.cs
+++ CB_GlobalGJ2022/Assets/Scripts/CameraManager.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    Camera cam;
 
-
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.mouseScrollDelta.y < 0)
+
+        if (Input.mouseScrollDelta.y < 0 )
         {
-            GetComponent<Camera>().orthographicSize += 2;
+            if(cam.orthographicSize < 12 || Input.GetKey(KeyCode.LeftShift))
+            {
+                cam.orthographicSize += 2;
+            }
+                
         }
         else if (Input.mouseScrollDelta.y > 0)
         {
-            GetComponent<Camera>().orthographicSize -= 2;
+            if (cam.orthographicSize > 6 || Input.GetKey(KeyCode.LeftShift))
+            {
+                cam.orthographicSize -= 2;
+            }
         }
     }
 }
